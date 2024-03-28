d6ba7d67fa3a2cc0d58866bfae081e15f179a4e5
desa-thomas <desa2thomas@gmail.com>
2024-03-28 16:41:32 -0400

    Added a whole bunch of things

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 /*
 Setting up global variables 
 
-NOTE TO SELF: 
-!!!!!!!!!!!!!!!!!!!
-CHANGE ALL COORDINATE HANDLING FROM 
-string "row, col" 
-TO 
-array [row, col]
-array of integers^^^^
+TO DO: 
+finish making tile clicks smooth
 */
 let gameTable = document.getElementById("game table"); 
+let headTable = document.getElementById("head table"); 
 let tableBody; 
 let bombArray = []; 
 let row; 
 let col; 
+
+//game header elements 
+let resetButton = document.getElementById("resetButton"); 
+let timerImgs = []; 
+let bombCountImgs = []; 
+resetButton.onmouseup = reset; 
+resetButton.onmousedown = function(){resetButton.src = "./assets/resetButtonClicked.png"}; 
+
 
 //setup difficulty selection buttons
 let selectedButton; 
@@ -23,6 +27,15 @@ for(let i = 0; i < buttons.length; i++){
 }
 let buttonBackgroundColor = getComputedStyle(buttons[0]).getPropertyValue('background-color'); 
 let buttonSelectColor = 'rgb(165, 156, 118)'; 
+
+//mouse listener reveal if left click, flag for anything else 
+let leftclick = 0; 
+let rightclick = 0;
+document.body.onmousedown = function (evt){
+    evt.button == 0 ? ++leftclick : ++rightclick}
+
+document.body.onmouseup = function (evt){
+    evt.button == 0 ? --leftclick : --rightclick}
 
 /*
 Function to generate an array of randomly generated coordinates of bombs on board. 
@@ -61,20 +74,7 @@ function createTable(){
 
         for(let j = 0; j < col; j++){
             const cell = document.createElement('td'); 
-            
-            //put default tile image 
-            const image = document.createElement('img'); 
-            image.src = "./assets/default.png"; 
-            image.width = 24;
-            image.height = 24; 
-            cell.appendChild(image); 
-
-            //make tile id its coordinates 
-            cell.id = i +", "+ j; 
-
-            //set up left and right click on each cell
-            cell.oncontextmenu = function (){}
-            cell.onclick = function(){}
+            createCell(cell, i, j); 
 
             //append cell to row 
             row.appendChild(cell); 
@@ -82,9 +82,47 @@ function createTable(){
         }
         tableBody.appendChild(row); 
     }
-
     gameTable.appendChild(tableBody); 
+
+    document.getElementById("timerSection").style.width = col*26 +4 +"px";
+   
+}
+
+/*
+Helper function to make each cell of gameTable, called by createTable 
+@param cell: cell DOM element <td> 
+@param i: Row
+@param j: column 
+*/
+function createCell(cell,i , j){
+    const image = document.createElement('img');
+    image.draggable = false;  
+    image.src = "./assets/default.png"; 
+    image.width = 24;
+    image.height = 24; 
+    cell.appendChild(image); 
+
+    //make tile id its coordinates 
+    let coords = i + ", " + j; 
+    image.id = coords; 
+
+    //clicking animation
+    cell.onmouseenter = function(){
+        if(leftclick)
+            image.src = "../assets/defaultClicked.png"; }
     
+    cell.onmousedown = function(){
+        if(rightclick ==0 )
+            image.src = "../assets/defaultClicked.png"; 
+    }
+    cell.onmouseleave = function(){
+        if(leftclick){
+            image.src = "./assets/default.png";}}
+
+    //left click reveal right click flag
+    cell.onmouseup = function(){reveal(image);}
+
+    cell.oncontextmenu = function(){flag(image);}
 }
 
 /*
@@ -120,9 +158,13 @@ Function for starting the game, used in buttonHandler once difficulty is selecte
 the start button is pushed
 */
 function startGame(){
+    //make table clickable 
+    gameTable.style.pointerEvents = 'auto'; 
+
     if(tableBody){
-        gameTable.removeChild(tableBody); 
+        gameTable.removeChild(tableBody);  
         tableBody = undefined; 
+        header = undefined; 
     }
 
     switch(selectedButton.id){
@@ -152,18 +194,45 @@ function startGame(){
             break; 
     }
     
+    document.getElementById("timerSection").style.display = "block"; 
+}
+
+/*
+reset game, set to reset button 
+*/
+function reset(){
+    resetButton.src = "./assets/resetButton.png"; 
+    console.log("reset"); 
 }
 
 /*
 function for flagging tiles. This function is assigned to the rightclick of 
 each <td> in the gameTable. 
-@param coords: The coordinates of the tile calling the function as the string "row, col"
+@param tile: tile image stored in cell 
 */
-function flag(coords){}
+function flag(tile){
+    // console.log("enabled table");
+    // gameTable.style.pointerEvents = 'auto';  
+
+    if (tile.class == "flagged"){
+        tile.src = "./assets/default.png"; 
+        tile.class = undefined; 
+    }
+    
+    else{
+        tile.class = "flagged"; 
+        tile.src = "./assets/flagged.png"; 
+    }
+}
 
 /*
 Function for revealing tiles. This function is assigned to the leftclick of each 
 <td> element in the gameTable.
-@param coords: The coordinates of the tile calling the function as the string "row, col"
+@param tile: tile image stored in cell 
 */
-function reveal(coords){}
+function reveal(tile){
+    // console.log("disabled table"); 
+    // gameTable.style.pointerEvents = 'none'; 
+     console.log(tile.id); 
+}
+
