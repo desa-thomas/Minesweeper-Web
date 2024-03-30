cef71726784e3165b5650835dd1ddbfebc3cf013
desa-thomas <desa2thomas@gmail.com>
2024-03-30 12:34:41 -0400

    Added timer, win conditions, and bomb counter

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ set winning conditions
 */
 let gameTable = document.getElementById("game table"); 
 let tableBody; 
+//array of bomb coordinates in form "row, col"
 let bombArray = []; 
 let row; 
 let col; 
-let ttlBombs; 
-let bombCount; 
 
 //timer section elements 
 let resetButton = document.getElementById("resetButton"); 
@@ -25,9 +24,14 @@ resetButton.onmousedown = function(){resetButton.src = "./assets/resetButtonClic
 resetButton.onmouseleave = function(){resetButton.src = "./assets/resetButton.png";}
 resetButton.draggable = false; 
 
-let hours; 
-let mins; 
-let secs;  
+let interval; 
+let firstClick; 
+let secs = 1;  
+
+//total amount of bombs on board, number of flagged tiles, and number of revealed tiles 
+let ttlBombs; 
+let flagCount = 0; 
+let ttlRevealed = 0; 
 
 //setup difficulty selection buttons
 let selectedButton; 
@@ -42,6 +46,9 @@ let buttonSelectColor = 'rgb(165, 156, 118)';
 gameTable.onmousedown = function(evt){clickHandler(evt);}
 gameTable.onmouseup = function(evt){clickHandler(evt);}
 
+buttons[1].click(); 
+buttons[buttons.length-1].click(); 
+
 /*
 Function to generate an array of randomly generated coordinates of bombs on board. 
 The array bombArray contains coordinates of bombs as the string "row, col" 
@@ -49,6 +56,7 @@ The array bombArray contains coordinates of bombs as the string "row, col"
 */
 function createBombArray(bombs){
     ttlBombs = bombs; 
+    bombArray = []; 
     let i = 0; 
     while(i < bombs){
         let rowCoord = Math.floor(Math.random()*(row)); 
@@ -59,14 +67,13 @@ function createBombArray(bombs){
             continue; 
 
         bombArray.push(coords);
-        let bombTile = document.getElementById(coords); 
-        Object.defineProperty(bombTile, "isBomb", {value:true,writeable:false,});
-
+        let bombTile = document.getElementById(coords);
         //define a property called isBomb for all bomb tiles and set to true; 
+        Object.defineProperty(bombTile, "isBomb", {value:true,writeable:false,});
         i++; 
     }
 
-    console.log(bombArray); 
+    updateBombCount(); 
 }
 
 /*
@@ -141,8 +148,8 @@ function buttonHandler(button){
         buttons[buttons.length-1].disabled = true; 
     }
 
-    else{
-        startGame(); 
+    else {
+        reset();  
     }   
 }
 
@@ -169,16 +176,19 @@ function clickHandler(evt){
             if(evt.button == 0 && tile.class != "flagged"){
                 tile.src = "./assets/defaultClicked.png"; 
             }
-            //on right click 
+            //on right click update flag count  
             if(evt.button ==2){
                 if(tile.class != "flagged"){
                     tile.src = "./assets/flagged.png"; 
                     tile.class = "flagged"; 
+                    flagCount ++; 
                 }
                 else{
                     tile.src = "./assets/default.png"; 
                     tile.class = ""; 
-                }  
+                    flagCount--; 
+                }
+                updateBombCount();   
             }
         }
         //When releasing, only reveal if it was a left click 
@@ -209,94 +219,112 @@ Function for starting the game, used in buttonHandler once difficulty is selecte
 the start button is pushed
 */
 function startGame(){
+    flagCount = 0; 
+    ttlRevealed = 0; 
+    secs = 1;  
+    for(let i = 0; i < timerImgs.length; i++){
+        timerImgs[i].src = "./assets/digit0.png"; 
+    }
     //make table clickable 
     gameTable.style.pointerEvents = 'auto'; 
 
     if(tableBody){
         gameTable.removeChild(tableBody);  
         tableBody = undefined; 
-        header = undefined; 
     }
+    if(selectedButton){
+        switch(selectedButton.id){
+            case "easy": 
+                row = 9; 
+                col = 9; 
+                createTable();
+                createBombArray(10);  
+                break; 
+            
+            case "medium": 
+                row = 16; 
+                col = 16; 
+                createTable() 
+                createBombArray(40); 
+                break; 
 
-    switch(selectedButton.id){
-        case "easy": 
-            row = 9; 
-            col = 9; 
-            createTable();
-            createBombArray(10);  
-            break; 
-        
-        case "medium": 
-            row = 16; 
-            col = 16; 
-            createTable() 
-            createBombArray(40); 
-            break; 
-
-        case "hard": 
-            row = 16; 
-            col = 30; 
-            createTable(); 
-            createBombArray(99); 
-            break; 
-        
-        case "custom": 
-            console.log("custom"); 
-            break; 
-    }
-    
-    document.getElementById("timerSection").style.display = "block"; 
+            case "hard": 
+                row = 16; 
+                col = 30; 
+                createTable(); 
+                createBombArray(99); 
+                break; 
+            
+        }
+        document.getElementById("timerSection").style.display = "block"; 
+    }   
+    else 
+        document.getElementById("timerSection").style.display = "none";
 }
 
-/*
-*/
-function startTimer(){}
 /*
 Function for revealing tiles. This function is assigned to the leftclick of each 
 <td> element in the gameTable.
 @param tile: tile image stored in cell 
 */
-function reveal(tile){
-    // console.log("disabled table"); 
-    // gameTable.style.pointerEvents = 'none'; 
-     console.log(tile.id); 
+function reveal(tile){ 
+    if(ttlRevealed==0)
+        firstClick = true; 
 
-     if(tile.isBomb){
-        tile.src = "./assets/bombClicked.png"; 
-        endGame(tile); 
-     }
-
-    else{
+    //if bomb end game
+    if(tile.isBomb){
+        loseGame(tile); 
+    }
+    //else reveal tile 
+    else {
         let adj = checkAdjacency(tile);
         if(adj != 0){
             tile.class = "revealed"; 
-            tile.src = "./assets/adj"+adj+".png"; 
+            tile.src = "./assets/adj"+adj+".png";
+            ttlRevealed++;  
         }
         else{
             recursiveReveal(tile)
         }
+
+        if(firstClick){
+            timerImgs[2].src = "./assets/digit1.png"; 
+            interval = setInterval(updateTimer, 1000); 
+            firstClick = false; 
+        }
+    
+        //if all tiles revealed end game 
+        else if(ttlRevealed == (row*col)-ttlBombs){
+            winGame();
+        }
     }
 }
-
+/*
+helper function to recursively reveal tiles, used when a tile adjacent to no bombs is clicked
+*/
 function recursiveReveal(tile){
     let coordArr = tile.id.split(",",2); 
     let tileRow = parseInt(coordArr[0]); 
-    let tileCol = parseInt(coordArr[1]); 
+    let tileCol = parseInt(coordArr[1]);
+
+    tile.class = "revealed"; 
+    tile.src = "./assets/defaultClicked.png"; 
+    ttlRevealed++; 
+
     for(let i = -1; i <= 1; i++){
         for(let j = -1; j <=1; j++){
             let coords = (tileRow+i) + ", " + (tileCol+j); 
             let adjTile = document.getElementById(coords);  
 
             if (adjTile != null && adjTile.class != "revealed"){
-                let adj = checkAdjacency(tile); 
-                tile.class = "revealed"; 
-
+                let adj = checkAdjacency(adjTile);  
                 if (adj == 0){
-                    tile.src = "./assets/defaultClicked.png"; 
                     recursiveReveal(adjTile); 
                 }
                 else{
-                    tile.src = "./assets/adj"+adj+".png"; 
+                    adjTile.src = "./assets/adj"+adj+".png"; 
+                    adjTile.class = "revealed";
+                    ttlRevealed++; 
                 }
             }
         }
@@ -304,6 +332,9 @@ function recursiveReveal(tile){
 
 }
 
+/*
+Helper function to check the number of bombs that a tile is adjacent to
+*/
 function checkAdjacency(tile){
     let coordArr = tile.id.split(",",2); 
     let tileRow = parseInt(coordArr[0]); 
@@ -323,20 +354,105 @@ function checkAdjacency(tile){
     return adj; 
 }
 
-function endGame(bombClicked){
+function firstClickSafe(bomb){}
+/*
+Function called when player loses game 
+@param bombClicked: tile of bomb user clicked
+*/
+function loseGame(bombClicked){
+    clearInterval(interval); 
     gameTable.style.pointerEvents = "none";
-    resetButton.src = "./assets/loseSmile.png"; 
+    bombClicked.src = "./assets/bombClicked.png"; 
+    resetButton.src = "./assets/loseSmile.png";
 
     for(let i = 0; i < bombArray.length; i++){
         let bomb = document.getElementById(bombArray[i]); 
-        if(bomb != bombClicked)
+        if(bomb != bombClicked &&bomb.class != "flagged")
             bomb.src = "./assets/bomb.png"; 
     }
 }
+
+function winGame(){
+    clearInterval(interval); 
+    resetButton.src = "./assets/winSmile.png";
+    gameTable.style.pointerEvents = "none";
+
+    for(let i = 0; i < bombArray.length; i++){
+        let bomb = document.getElementById(bombArray[i]); 
+        bomb.src = "./assets/flagged.png"; 
+    } 
+    
+}
+
+/*
+function used to update bomb counter on top left of the timer section. 
+This function is called by click handler to update it any time user right clicks, also used by
+CreateBombArray to set the counter to the number of bombs at start of game. 
+    - The count represents #of bombs - #of flags, so you know how many bombs you have left to flag, 
+      although the game does not end until you have revealed all tiles that are not bombs 
+*/
+function updateBombCount(){
+    let bombCount = (ttlBombs-flagCount)
+    let bombCountArr = (bombCount+"").split("");
+    let hundred = "0"; 
+    let ten = "0"; 
+    let one = "0"; 
+
+    if(bombCountArr.length == 3){
+        hundred = bombCountArr[0]; 
+        ten = bombCountArr[1]; 
+        one = bombCountArr[2]; 
+    }
+    else if(bombCountArr.length == 2){
+        if(bombCount < 0)
+            hundred =""; 
+        ten = bombCountArr[0]; 
+        one = bombCountArr[1]; 
+    }
+    else
+        one = bombCountArr[0]; 
+
+    bombCountImgs[0].src = "./assets/digit"+hundred+".png"; 
+    bombCountImgs[1].src = "./assets/digit"+ten+".png"; 
+    bombCountImgs[2].src = "./assets/digit"+one+".png"; 
+}
+
+/*
+Function to update timer on the right side of timer section. Function is called when player reveals first tile on board, and is called 
+every second. Timer is ended when player wins game or when player loses game. Timer is reset to 0 with reset function.
+*/
+function updateTimer(){
+    secs++; 
+
+    if(secs < 1000){
+        let secArr = (secs+"").split(""); 
+        let h = "0"; 
+        let t = "0"; 
+        let o = "0"; 
+        
+        if(secArr.length == 3){
+            h = secArr[0]; 
+            t = secArr[1]; 
+            o = secArr[2]; 
+        }
+        else if(secArr.length == 2){
+            t = secArr[0]; 
+            o = secArr[1]; 
+        }
+        else
+            o = secArr[0]; 
+
+        timerImgs[0].src = "./assets/digit"+h+".png"; 
+        timerImgs[1].src = "./assets/digit"+t+".png"; 
+        timerImgs[2].src = "./assets/digit"+o+".png"; 
+    }
+}
+
 /*
 reset game, set to reset button 
 */
 function reset(){
+    clearInterval(interval); 
     resetButton.src = "./assets/resetButton.png"; 
     startGame(); 
 }
